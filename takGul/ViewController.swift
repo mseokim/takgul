8cb695f362adaa5ea34bece91897f1fda96f5710
mseokim <min101110001@icloud.com>
2017-08-06 22:29:39 +0900

    start

---
+++ takGul/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  takGul
+//
+//  Created by 김민서 on 2017. 8. 6..
+//  Copyright © 2017년 김민서. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
